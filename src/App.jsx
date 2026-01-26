e4702970733f9b2046eca0052b113d7940f02435
Swayam-The Coder <swayamcode1@gmail.com>
2026-01-26 22:50:19 +0530

    added on input

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,24 @@ import Errormsg from "./components/Food/Errormsg";
 import Foodinput from "./components/Food/Foodinput";
 const App = () => {
   // const foodItems = [];
-  const foodItems = ["Dal", "Rice", "Chapati", "Vegetables", "Curry"];
-  
-  let textState = useState("Welcome to Healthy Food App");
-  let texttoshow = textState[0];
-  let settextshow = textState[1];
+  let [foodItems , setfoodItem] = useState([]);
 
-  const handlechange = (event) => {
-    console.log(event.target.value)
-    settextshow(event.target.value)
-  }
+  const Keydown = (event) => {
+    if(event.key === "Enter"){
+      let newitems = event.target.value;
+      let updatedfooditems = [...foodItems , newitems];
+      setfoodItem(updatedfooditems);
+      event.target.value = "";
+    }
 
+}
   return (
     <div>
       <div className="border border-gray-400 m-4 p-4 w-sm min-w-0 mx-auto bg-yellow-50">
         <h1 className="text-center p-3 text-red-700 font-medium text-2xl">
           Healthy Food
         </h1>
-        <Foodinput handlechange={handlechange}></Foodinput>
-        <p>{texttoshow}</p>
+        <Foodinput Keydown={Keydown}></Foodinput>
         <Errormsg items={foodItems}></Errormsg>
         <FoodItems items={foodItems}></FoodItems>
       </div>
--- src/components/Food/Foodinput.jsx
+++ src/components/Food/Foodinput.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Foodinput = ( {handlechange} ) => {
+const Foodinput = ( {Keydown} ) => {
 
   return (
     <input
       type="text"
       className="border border-gray-400 p-2 rounded-md w-xs min-w-0 mx-auto mt-4 mb-4 ml-4 outline-0 bg-yellow-200"
       placeholder="Enter food item here"
-      onChange={handlechange}
+      onKeyDown={Keydown}
     />
   );
 };
